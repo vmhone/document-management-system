a53a3c7e192a6acd41b2193957e8357154b6285e
Vitu Mhone <mhone.vitu@gmail.com>
2022-04-05 11:12:16 +0200

    Updated the version to 4.1.9 from 4.1.7

--- app/Views/Common/InitiateReset.php
+++ app/Views/Common/InitiateReset.php
@@ -6,7 +6,7 @@
     <title>Reset Password | Document Management System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Document Management System" name="description" />
-    <meta content="IC-Tech Africa" name="author" />
+    
     <!-- App favicon -->
     <link rel="shortcut icon" href="<?php echo base_url() ?>/assets/images/favicon.ico">
 
